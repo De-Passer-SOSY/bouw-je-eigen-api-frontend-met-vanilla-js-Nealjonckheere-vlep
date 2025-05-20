b27db0391444613975664cc7b35cc70eecffcba5
Nealjonckheere-vlep <neal.jonckheere@leerling.de-passer.be>
2025-05-20 14:40:05 +0200

    Database Setup
    Functies in java aan het klaar zetten voor
    database :)

--- Frontend/assets/js/script.js
+++ Frontend/assets/js/script.js
@@ -1,4 +1,30 @@
 "use strict";
 
 
-//Helemaal zelf
+//Helemaal zelf GEBRUIK PULL FROM DATABASE SLIMME AAP
+document.addEventListener("DOMContentLoaded", init)
+
+function init(){
+    pokemon()
+    console.log("Website Werd geladen")
+}
+
+async function pokemon(){
+    let response = await fetch('https://localhost:3333' + benodiged_functie + PKid )
+    let data = await response.json();
+}
+
+
+function benodiged_functie (){
+
+}
+
+function PKid (){
+
+
+}
+
+// extra's als ik tijd over heb deze voor flits bij de drie gekleurde bolletjes
+function Flash (){
+    document.getElementById('foto-zone').style.backgroundColor = kleur
+}
